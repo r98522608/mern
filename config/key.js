e362975579ec9a338214c7593772c74c2415e156
r98522608 <r98522608@ntu.edu.tw>
2018-08-18 17:51:42 +0800

    1st commit

---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    mongoURI : 'mongodb://ives:fuck@localhost/mern'
+    //mongoURI = 'mongodb://localhost:27017/mern',
+};
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose =require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+
+// DB config
+//const db = require('/home/ives/code/MERN/config/keys,js').mongoURI;
+const db = require('./config/key').mongoURI;
+
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected....'))
+    .catch(err => console.log(err));
+
+//mongoose.connect('mongodb://localhost/test')
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
